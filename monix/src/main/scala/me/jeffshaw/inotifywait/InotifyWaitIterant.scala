ed9530020d30dc5eaad7313271080ec48e3ddfed
Jeff Shaw <shawjef3@gmail.com>
2019-11-21 01:17:55 -0500

    simplify monix implementation
    add iterant unit test

--- monix/src/main/scala/me/jeffshaw/inotifywait/InotifyWaitIterant.scala
+++ monix/src/main/scala/me/jeffshaw/inotifywait/InotifyWaitIterant.scala
@@ -1,38 +1,27 @@
 package me.jeffshaw.inotifywait
 
-import cats.Applicative
 import cats.effect.Sync
 import java.nio.file.Path
 import monix.tail.Iterant
 
 object InotifyWaitIterant {
-  def toIterant[F[_]](
-    i: InotifyWait
-  )(implicit F: Applicative[F]
-  ): Iterant[F, Events] = {
-    Iterant.fromIterator(i.events)
-  }
-
-  def acquire[F[_]](
+  def apply[F[_]](
     path: Path,
     recursive: Boolean,
     subscriptions: Set[Subscription]
-  )(implicit F: Sync[F]
-  ): Iterant[F, InotifyWait] = {
-    Iterant.resource(
-      F.delay(InotifyWait.start(path, recursive, subscriptions))
-    )(i => F.delay {
-      i.process.destroy()
-      i.process.waitFor()
-    })
-  }
-
-  def start[F[_]](
-    path: Path,
-    recursive: Boolean,
-    subscriptions: Set[Subscription]
-  )(implicit F: Sync[F]
+  )(implicit F: Sync[F],
+    codec: io.Codec
   ): Iterant[F, Events] = {
-    acquire(path,recursive, subscriptions).flatMap(toIterant)
+    val processI =
+      Iterant.resource(
+        F.delay(new ProcessBuilder(InotifyWait.command(path, recursive, subscriptions): _*).start())
+      )(process => F.delay {
+        process.destroy()
+        process.waitFor()
+      })
+    for {
+      process <- processI
+      line <- Iterant.fromIterator(io.Source.fromInputStream(process.getInputStream).getLines())
+    } yield Events.valueOf(line)
   }
 }
